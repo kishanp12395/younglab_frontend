3e94497ca48ede820d61e5eb3b56135ba4257056
kishanp12395 <kkp12395@gmail.com>
2025-03-07 00:11:08 +0530

    frontend repo route modified

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<UserSearch />} />
           <Route path="/add" element={<AddUser />} />
-          <Route path="/*" element={<NotFound404/>} />
+          <Route path="*" element={<NotFound404/>} />
         </Routes>
       </div>
     </Router>
